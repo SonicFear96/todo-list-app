2c2b8e643205f67c2f64b7c3904633f34b64ee2b
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-09-18 17:05:13 +0300

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  vertical-align: baseline;
+  font-family: inherit;
+  font-style: inherit;
+  font-size: 100%;
+  border: none;
+  padding: 0;
+  margin: 0;
 }
 
-nav {
-  padding: 30px;
+*, *::before, *::after {
+  box-sizing: border-box;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+html, body {
+  margin: 0;
+  padding: 0;
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
+button {
+  font-family: initial;
+}
+
+img {
+  display: block;
+  max-width: 100%;
+  max-height: 100%;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+}
+
+a img, :link img, :visited img {
+  border: 0;
 }
 </style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page home-page">
+    it's home page
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home-page',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+  .page.home-page {
+    
+}
+</style>
