9f7ae909517e26464c1977af56540f7a126d57f8
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-09-18 22:24:00 +0300

    feature: change title (input)

--- src/components/card/index.vue
+++ src/components/card/index.vue
@@ -3,8 +3,11 @@
     <div v-if="showTitle" class="title">
         {{ data.title }}
     </div>
-    <div v-else>
-      <input :value="data.title" @input="input">
+    <div v-else class="input-wrapper">
+      <input class="card-input" :value="data.title" @input="input"> 
+      <button class="button" @click="changePost">
+        <iconCheck />
+      </button>
     </div>
     <ul v-if="data.items.length" class="todo-list">
       <li class="todo-item" v-for="(item, index) in data.items" :key="index"> {{index + 1}} {{ item }} </li>
@@ -13,16 +16,23 @@
       к сожалению, список дел ещё не заполнен
     </div>
     <div class="button-wrapper">
-      <button class="button" @click="changeData">
+      <button class="button" @click="showChangePost">
         изменить 
+      </button>
+      <button class="button" @click="deletePost">
+        удалить
       </button>
     </div>
  </div>   
 </template>
 
 <script>
+import iconCheck from '@/components/icons/check'
 export default {
   name: 'card-component',
+  components: {
+    iconCheck
+  },
   props: {
     data: {
       type: Object,
@@ -36,14 +46,18 @@ export default {
     }
   },
   methods: {
-    changeData () {
-      if (this.showTitle) {
-        this.showTitle = false
-      } else {
+    showChangePost () {
+      this.showTitle = false
+    },
+    changePost () {
+      if (this.newTitle !== '') {
         this.data.title = this.newTitle
         this.$store.dispatch('changePost', this.data)
         this.showTitle = true
       }
+    },
+    deletePost () {
+      this.$store.dispatch('deletePost', this.data)
     },
     input () {
       this.newTitle = event.target.value
@@ -60,8 +74,28 @@ export default {
   height: 100%;
 }
 .title {
+    height: 40px;
     font-size: 24px;
     font-weight: bold;
+}
+.card-input {
+  height: 28px;
+  border: 1px solid gray;
+}
+.input-wrapper {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  height: 40px;
+}
+.input-wrapper .button {
+  width: 45px;
+  padding: 5px;
+}
+.input-wrapper .button svg {
+  width: 18px;
+  height: 18px;
+  fill: white;
 }
 .todo-list {
   margin-top: 10px;
@@ -70,5 +104,11 @@ export default {
 .todo-item, .todo-item--no {
   font-size: 18px;
   color: grey;
+}
+.button-wrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+  margin-top: 20px;
 }
 </style>
