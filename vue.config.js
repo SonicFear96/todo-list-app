3060bac88d7711a56f7ccf81f5d80037c4e0d874
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-09-18 18:16:29 +0300

    deploy final

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-// const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true,
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/todo-list-app/'
-//     : '/'
-// })
 module.exports = {
   publicPath: '/todo-list-app/'
 }
